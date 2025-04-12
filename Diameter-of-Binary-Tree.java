f369a8b05970ff3d796e5acd722d1ab402bd0434
Chandini Gujju <168464128+GujjuChandini@users.noreply.github.com>
2025-04-12 07:23:14 +0530

    [543] [Time Beats: 100.00%] [Memory Beats: 21.15%] - LeetPush

--- Diameter-of-Binary-Tree.java
+++ Diameter-of-Binary-Tree.java
@@ -14,21 +14,21 @@
  * }
  */
 class Solution {
-    private int diameter = 0;
+    public int D = 0;
+    public int height(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        int left = height(root.left);
+        int right = height(root.right);
 
+        int currD = left+right;
+        D = Math.max(D, currD);
+
+        return 1 + Math.max(left, right);
+    }
     public int diameterOfBinaryTree(TreeNode root) {
         height(root);
-        return diameter;
-    }
-
-    private int height(TreeNode node) {
-        if (node == null) return 0;
-
-        int leftHeight = height(node.left);
-        int rightHeight = height(node.right);
-
-        diameter = Math.max(diameter, leftHeight + rightHeight);
-
-        return 1 + Math.max(leftHeight, rightHeight);
+        return D;
     }
 }
