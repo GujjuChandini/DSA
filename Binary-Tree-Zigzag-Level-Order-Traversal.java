b5a38b3ff28728e8f7bbec4036d4f3f53073b725
Chandini Gujju <168464128+GujjuChandini@users.noreply.github.com>
2025-04-15 07:30:48 +0530

    [103] [Time Beats: 100.00%] [Memory Beats: 39.73%] - LeetPush

--- Binary-Tree-Zigzag-Level-Order-Traversal.java
+++ Binary-Tree-Zigzag-Level-Order-Traversal.java
@@ -1,34 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
-        if (root == null) return result;
-
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
+        if(root==null) return result;
         boolean leftToRight = true;
-
-        while (!queue.isEmpty()) {
-            int levelSize = queue.size();
-            LinkedList<Integer> level = new LinkedList<>();
-
-            for (int i = 0; i < levelSize; i++) {
-                TreeNode current = queue.poll();
-                
-                // Add the value based on the direction
-                if (leftToRight) {
-                    level.addLast(current.val);
-                } else {
-                    level.addFirst(current.val);
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            List<Integer> answer = new LinkedList<>();
+            int size = q.size();
+            for(int i=0;i<size;i++){
+                TreeNode curr = q.poll();
+                if(leftToRight){
+                    if(curr!=null)
+                        answer.addLast(curr.val);
+                }else{
+                    if(curr!=null)
+                        answer.addFirst(curr.val);
                 }
-
-                if (current.left != null) queue.offer(current.left);
-                if (current.right != null) queue.offer(current.right);
+                if(curr.left!=null) q.offer(curr.left);
+                if(curr.right!=null) q.offer(curr.right);
             }
 
-            result.add(level);
-            leftToRight = !leftToRight;  // Flip direction
+            result.add(answer);
+            leftToRight = !leftToRight;
         }
-
         return result;
     }
 }
