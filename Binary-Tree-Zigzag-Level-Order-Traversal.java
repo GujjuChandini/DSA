1c3601598f034085e27737a573d475a954f44628
Chandini Gujju <168464128+GujjuChandini@users.noreply.github.com>
2025-04-29 11:44:41 +0530

    [103] [Time Beats: 100.00%] [Memory Beats: 24.85%] - LeetPush

--- Binary-Tree-Zigzag-Level-Order-Traversal.java
+++ Binary-Tree-Zigzag-Level-Order-Traversal.java
@@ -16,28 +16,26 @@
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
-        if(root==null) return result;
-        boolean leftToRight = true;
+        if (root == null) return result;
+        boolean ltr = true;
         Queue<TreeNode> q = new LinkedList<>();
         q.offer(root);
-        while(!q.isEmpty()){
-            List<Integer> answer = new LinkedList<>();
+        while (!q.isEmpty()) {
             int size = q.size();
-            for(int i=0;i<size;i++){
-                TreeNode curr = q.poll();
-                if(leftToRight){
-                    if(curr!=null)
-                        answer.addLast(curr.val);
-                }else{
-                    if(curr!=null)
-                        answer.addFirst(curr.val);
+            List<Integer> answer = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                if (ltr) {
+                    answer.add(node.val);
+                } else {
+                    answer.add(0, node.val);
                 }
-                if(curr.left!=null) q.offer(curr.left);
-                if(curr.right!=null) q.offer(curr.right);
+                if (node.left != null) q.offer(node.left);
+                if (node.right != null) q.offer(node.right);
             }
 
             result.add(answer);
-            leftToRight = !leftToRight;
+            ltr = !ltr;
         }
         return result;
     }
