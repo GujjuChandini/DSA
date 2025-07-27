c1e2f310e80d30cb33fec7962db86fe5e69c14f4
Chandini Gujju <168464128+GujjuChandini@users.noreply.github.com>
2025-07-27 12:40:30 +0530

    [112] [Time Beats: 100.00%] [Memory Beats: 35.82%] - LeetPush

--- Path-Sum.java
+++ Path-Sum.java
@@ -15,15 +15,13 @@
  */
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        if (root == null) {
+        if(root==null){
             return false;
         }
-
-        if (root.left == null && root.right == null) {
+        if(root.left==null && root.right==null){
             return targetSum == root.val;
         }
-
-        int remainingSum = targetSum - root.val;
-        return hasPathSum(root.left, remainingSum) || hasPathSum(root.right, remainingSum);
+        int remaining = targetSum - root.val;
+        return hasPathSum(root.left, remaining) || hasPathSum(root.right, remaining);
     }
 }
