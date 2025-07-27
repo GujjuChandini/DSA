179e1f797260d7c8d0120d072a35cc4bc1b9c6f5
Chandini Gujju <168464128+GujjuChandini@users.noreply.github.com>
2025-07-27 11:26:44 +0530

    [236] [Time Beats: 100.00%] [Memory Beats: 72.60%] - LeetPush

--- Lowest-Common-Ancestor-of-a-Binary-Tree.java
+++ Lowest-Common-Ancestor-of-a-Binary-Tree.java
@@ -9,19 +9,14 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null) return null;
-
-        if (root.val == p.val || root.val == q.val) {
+        if(root == null || root == p || root == q){
             return root;
         }
-
-        TreeNode leftLCA = lowestCommonAncestor(root.left, p, q);
-        TreeNode rightLCA = lowestCommonAncestor(root.right, p, q);
-
-        if (leftLCA != null && rightLCA != null) {
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if(left != null && right != null){
             return root;
         }
-
-        return leftLCA != null ? leftLCA : rightLCA;
+        return (left!=null) ? left : right;
     }
 }
