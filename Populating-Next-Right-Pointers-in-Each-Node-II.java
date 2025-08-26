7402df252137a70147a714209d24935bb66ba4f8
Chandini Gujju <168464128+GujjuChandini@users.noreply.github.com>
2025-08-26 15:41:22 +0530

    [117] [Time Beats: 70.06%] [Memory Beats: 28.18%] - LeetPush

--- Populating-Next-Right-Pointers-in-Each-Node-II.java
+++ Populating-Next-Right-Pointers-in-Each-Node-II.java
@@ -21,30 +21,23 @@ class Node {
 };
 */
 
-import java.util.*;
-
 class Solution {
     public Node connect(Node root) {
         if (root == null) return null;
-
         Queue<Node> q = new LinkedList<>();
         q.offer(root);
-
-        while (!q.isEmpty()) {
+        while(!q.isEmpty()){
             int size = q.size();
             Node prev = null;
-
-            for (int i = 0; i < size; i++) {
-                Node node = q.poll();
-
-                if (prev != null) {
-                    prev.next = node;
+            for(int i=0;i<size;i++){
+                Node curr = q.poll();
+                if(prev!=null){
+                    prev.next = curr;
                 }
-                prev = node;
-
-                if (node.left != null) q.offer(node.left);
-                if (node.right != null) q.offer(node.right);
-            }
+                prev = curr;
+                if(curr.left != null) q.offer(curr.left);
+                if(curr.right != null) q.offer(curr.right);
+            } 
         }
         return root;
     }
