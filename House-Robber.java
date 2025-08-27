5b3f85e90a5c028877e934aa8a985bbdcbec6535
Chandini Gujju <168464128+GujjuChandini@users.noreply.github.com>
2025-08-27 21:41:56 +0530

    [198] [Time Beats: 100.00%] [Memory Beats: 8.49%] - LeetPush

--- House-Robber.java
+++ House-Robber.java
@@ -1,9 +1,10 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp, -1);
-        return helper(nums, 0, dp, n);
+        // int[] dp = new int[n];
+        // Arrays.fill(dp, -1);
+        // return helper(nums, 0, dp, n);
+        return helper1(nums);
     }
     public int helper(int[] nums, int i, int[] dp, int n){
         if(i>=n) return 0;
@@ -13,5 +14,17 @@ class Solution {
         int take = nums[i]+helper(nums, i+2, dp, n);
         int nottake = helper(nums, i+1, dp, n);
         return dp[i] = Math.max(take, nottake);
+    }
+    public int helper1(int[] nums){
+        int n = nums.length;
+        if(n==0) return 0;
+        if(n==1) return nums[0];
+        int[] dp = new int[n+1];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0],nums[1]);
+        for(int i=2;i<n;i++){
+            dp[i] = Math.max(nums[i]+dp[i-2],dp[i-1]);
+        }
+        return dp[n-1];
     }
 }
