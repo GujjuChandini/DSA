6dcd7cfb79db41ecc7a6e959425ccd582416534a
Chandini Gujju <168464128+GujjuChandini@users.noreply.github.com>
2025-09-02 00:46:18 +0530

    [1312] [Time Beats: 11.01%] [Memory Beats: 53.04%] - LeetPush

---
+++ Minimum-Insertion-Steps-to-Make-a-String-Palindrome.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minInsertions(String s) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                dp[i][j] = -1;
+            }
+        }
+        StringBuilder sb = new StringBuilder(s);
+        return n-helper(s, sb.reverse().toString(), 0, 0, n, dp);
+    }
+    public int helper(String s1, String s2, int i, int j, int n, int[][] dp){
+        if(i>=n || j>=n){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        if(s1.charAt(i) == s2.charAt(j)){
+            dp[i][j] = 1+helper(s1, s2, i+1, j+1, n, dp);
+        }else{
+            dp[i][j] = Math.max(helper(s1, s2, i+1, j, n, dp),helper(s1,s2, i, j+1, n, dp));
+        }
+        return dp[i][j];
+    }
+}
