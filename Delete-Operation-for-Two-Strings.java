5c22a4201bb3e305364639e50fae6b0d4e33ac57
Chandini Gujju <168464128+GujjuChandini@users.noreply.github.com>
2025-09-02 01:02:30 +0530

    [583] [Time Beats: 68.56%] [Memory Beats: 36.35%] - LeetPush

--- Delete-Operation-for-Two-Strings.java
+++ Delete-Operation-for-Two-Strings.java
@@ -8,8 +8,7 @@ class Solution {
                 dp[i][j] = -1;
             }
         }
-        //return helper(word1, word2, n, m, dp, 0, 0);
-        return helper1(word1, word2);
+        return helper(word1, word2, n, m, dp, 0, 0);
     }
     public int helper(String word1, String word2, int n, int m, int[][] dp, int i, int j){
         if(i==n){
@@ -27,26 +26,5 @@ class Solution {
             dp[i][j] = 1+Math.min(helper(word1, word2, n, m, dp, i+1, j),helper(word1, word2, n, m, dp, i, j+1));
         }
         return dp[i][j];
-    }
-    public int helper1(String word1, String word2){
-        int n = word1.length();
-        int m = word2.length();
-        int[][] dp = new int[n+1][m+1];
-        for(int i=0;i<=n;i++){
-            dp[i][m] = n-i;
-        }
-        for(int i=0;i<=m;i++){
-            dp[n][i] = m-i;
-        }
-        for(int i=n-1;i>=0;i--){
-            for(int j=m-1;j>=0;j--){
-                if(word1.charAt(i)==word2.charAt(j)){
-                    dp[i][j] = dp[i+1][j+1];
-                }else{
-                    dp[i][j] = 1+ Math.min(dp[i+1][j],dp[i][j+1]);
-                }
-            }
-        }
-        return dp[0][0];
     }
 }
