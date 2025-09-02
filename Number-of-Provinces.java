48df4fc5007ce772b7bc74d2526e92080bcff7ac
Chandini Gujju <168464128+GujjuChandini@users.noreply.github.com>
2025-09-02 22:45:26 +0530

    [547] [Time Beats: 87.28%] [Memory Beats: 45.97%] - LeetPush

---
+++ Number-of-Provinces.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        int[] vis = new int[n];
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (vis[i] == 0) {
+                dfs(isConnected, vis, i);
+                ans++;
+            }
+        }
+        return ans;
+    }
+
+    private void dfs(int[][] isConnected, int[] vis, int node) {
+        vis[node] = 1;
+
+        for (int j = 0; j < isConnected.length; j++) {
+            if (isConnected[node][j] == 1 && vis[j] == 0) {
+                dfs(isConnected, vis, j);
+            }
+        }
+    }
+}
